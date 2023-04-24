56b193d550f99eec287d3cab164ffa2f8c81769e
Ana Paula dos Santos Marianni <ana.marianni@DESKTOP-P6LCRPC.zoomtecnologia.com.br>
2023-04-24 15:50:23 -0300

    adicionada rota post

--- mulheres.js
+++ mulheres.js
@@ -1,28 +1,70 @@
 const express = require('express');
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+app.use(express.json());
 const porta = 3342;
 
 const mulheres = [
     {
+    id: '1',
     nome: 'Simara Conceição',
     imagem: 'https://github.com/simaraconceicao.png',
     minibio: 'Desenvolvedora e instrutora'
 },
 {
+    id: '2',
     nome: 'Iana Chan',
     imagem: 'https://bit.ly/3LJIyOF',
     minibio: 'Desenvolvedora e instrutora'
 },
 {
+    id: '3',
     nome: 'Luana Pimentel',
     imagem: 'https://bit.ly/3FKpFaz',
     minibio: 'Desenvolvedora e instrutora'
 }
 ]
 
+//GET
 function mostraMulheres(request, response) {
+    response.json(mulheres);
+}
+//POST
+function criaMulher(request, response) {
+    const novaMulher = {
+        id: uuidv4(),
+        nome: request.body.nome,
+        imagem: request.body.imagem,
+        minibio: request.body.minibio
+    }
+
+    mulheres.push(novaMulher) 
+}
+
+//PATCH
+function corrigeMulher(request, response) {
+    function encontraMulher(mulher) {
+        if(mulher.id === request.params.id) {
+            return mulher
+        }
+    }
+
+    const mulherEncontrada = mulheres.find(encontraMulher)
+    
+    if(request.body.nome){
+        mulherEncontrada.nome = request.body.nome
+    }
+
+    if(request.body.minibio){
+        mulherEncontrada.minibio = request.body.minibio
+    }
+
+    if(request.body.imagem){
+        mulherEncontrada.imagem = request.body.imagem
+    }
+
     response.json(mulheres);
 }
 
@@ -31,4 +73,6 @@ function mostraPorta() {
 }
 
 app.use(router.get('/mulheres', mostraMulheres));
+app.use(router.post('/mulheres', criaMulher));
+app.use(router.patch('/mulheres/:id', corrigeMulher));
 app.listen(porta, mostraPorta);
